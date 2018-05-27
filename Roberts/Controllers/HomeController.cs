ec2a3aacb790db2ad94e179b1569e675939b3e77
Alisdair <Alisdair@LAPTOP-TKI3D179>
2018-05-28 11:51:03 +1200

    Email should work. Note should

--- Roberts/Controllers/HomeController.cs
+++ Roberts/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
 using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using Roberts.Models;
 
 namespace Roberts.Controllers
@@ -12,6 +14,22 @@ namespace Roberts.Controllers
     {
         public IActionResult Index()
         {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Test Project", "dksalazars@gmail.com"));
+            message.To.Add(new MailboxAddress("Gary", "i588191@nwytg.com"));
+            message.Subject = "ASP.NET Test";
+            message.Body = new TextPart("plain")
+            {
+                Text = "Hello World mail"
+            };
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("dksalazars@gmail.com", "amazingmerino42");
+                client.Send(message);
+                client.Disconnect(true);
+            }
+
             return View();
         }
 
